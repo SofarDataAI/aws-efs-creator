ac0893deaa8444506dea1de7795a119c60f6716e
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-19 15:50:31 +0700

    Add efsSG

--- lib/aws-efs-creator-stack.ts
+++ lib/aws-efs-creator-stack.ts
@@ -1,12 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { AwsEfsCreatorStackProps } from './AwsEfsCreatorStackProps';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsEfsCreatorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AwsEfsCreatorStackProps) {
     super(scope, id, props);
 
+    const vpc = ec2.Vpc.fromLookup(this, `${props.resourcePrefix}-VPC`, {
+      vpcId: props.vpcId,
+    });
     const removalPolicy = props.deployEnvironment === 'production' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY;
+
+    // define a security group for EFS
+    const efsSG = new ec2.SecurityGroup(this, `${props.resourcePrefix}-efsSG`, {
+      securityGroupName: `${props.resourcePrefix}-efsSG`,
+      vpc: vpc,
+      allowAllOutbound: true,
+    });
   }
 }
